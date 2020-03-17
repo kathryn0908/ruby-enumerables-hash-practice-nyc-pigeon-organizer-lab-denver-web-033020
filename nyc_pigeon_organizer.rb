b78835f8de7df03756511963c1c1f4a51a6b7dda
Kathryn Clark <kathrynlael@gmail.com>
2020-03-17 22:27:35 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,26 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  
+  names = []
+  pigeon_hash = {}
+  
+  data.each do |attribute, items|
+    items.each do |feature, arr|
+      arr.each do |name|
+    names << name if !names.include?(name)
+end
+end
+end
+
+names.each do |n|
+  pigeon_hash[n] = Hash.new {|k,v| k[v]=[]}
+   data.each do |attribute, items|
+     pigeon_hash[n][attribute]
+    items.each do |feature, arr|
+      arr.each do |name|
+        pigeon_hash[name][attribute] << feature.to_s if name == n 
+      end
+    end
+  end
+end
+pigeon_hash
 end
